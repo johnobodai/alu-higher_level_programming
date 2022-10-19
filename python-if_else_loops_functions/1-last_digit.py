c514c55b162c8e05e2d67520463609f52a54c4bd
johnobodai <j.obodai@alustudent.com>
2022-10-19 10:56:36 +0000

    Print last's digit's polarity

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+def pnivel(arg):
+    x = arg % 10
+    if x > 5:
+        print(f"Last digit of {arg} is {x} and is greater than 5")
+    elif x == 0:
+        print(f"Last digit of {arg} is {x} and is 0")
+    elif x < 6 & x != 0:
+        print(f"Last digit of {arg} is {x} and is less than 6 and not 0")
+
+pnivel(number)
+
