cc70db58fa42172b67460a4039ccf07ce7f91003
johnobodai <j.obodai@alustudent.com>
2022-10-19 10:09:20 +0000

    Print polatity of a number

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 import random
-
+"""this program prints the polority of a random number
+@arg is the nuber to be passed into the function"""
 
 number = random.randint(-10, 10)
+
+
 def pnive(arg):
+
     if arg > 0:
         print(f"{arg} is positive")
     elif arg < 0:
         print(f"{arg} is negative")
     else:
         print(f"{arg} is zero")
+
 
 pnive(number)
