59cb5ffd4d84d335e9d0a3e7a16b9c0e6108f933
johnobodai <j.obodai@alustudent.com>
2022-10-19 10:58:42 +0000

    Print polrity of last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
+
 def pnivel(arg):
     x = arg % 10
     if x > 5:
@@ -11,5 +12,5 @@ def pnivel(arg):
     elif x < 6 & x != 0:
         print(f"Last digit of {arg} is {x} and is less than 6 and not 0")
 
-pnivel(number)
 
+pnivel(number)
