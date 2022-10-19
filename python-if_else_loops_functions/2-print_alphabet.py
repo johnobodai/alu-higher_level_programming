6655aebbeb0969ca2706590ec3535aebd2acd95f
johnobodai <j.obodai@alustudent.com>
2022-10-19 20:17:51 +0000

    asciibets

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+# This code prints out all alpha in lowercase
+# @i - interger that runs through the ascii codes
+
 for i in range(97,123):
     print("{}".format(chr(i)),end='')
