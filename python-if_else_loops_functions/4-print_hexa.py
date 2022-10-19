44f163b3c008a7db92f81b31c58c023dbc94bd0c
johnobodai <j.obodai@alustudent.com>
2022-10-19 22:14:47 +0000

    Print numerals

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # This code converts form decimal sys to hexadecimal sys using the python function hex
-# @i iterates to 98 
+# @i iterates to 98
 for i in range(0, 99):
     print("{} = {}".format(i, hex(i)))
