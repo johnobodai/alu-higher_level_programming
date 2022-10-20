258d91f68eebfd6e98bb778ecaaa34787925e039
johnobodai <j.obodai@alustudent.com>
2022-10-20 07:43:25 +0000

    Check for case of input

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def _islower(c):
+    """Checks if the given argument is lowercase"""
+    if ord('c') >= 97 and ord('c') <= 123 :
+        return True
+    else:
+        return False
+
+
+_islower(c)
