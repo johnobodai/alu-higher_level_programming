d884fddc8c1d99d6aee4ca69098b6c19e015d70d
johnobodai <j.obodai@alustudent.com>
2022-10-20 06:54:36 +0000

    Combination without repetition

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ result = ""
 for i in range(8):
     for j in range(10):
         if j != i and j > i:
-            print("{}{}".format(i,j),end=',')
+            print("{}{}".format(i, j), end=', ')
 print("89")
