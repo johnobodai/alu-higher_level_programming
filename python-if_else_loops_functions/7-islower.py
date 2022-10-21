9408d776f8d3653822b8a49371801ef3b47d5dff
johnobodai <johnodaiobodai@gmail.com>
2022-10-21 00:55:35 +0000

    Islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""
 alq = input()
 alp = ord(alq)
 def islower(alp):
@@ -9,3 +10,10 @@ def islower(alp):
         print("not lower")
 
 islower(alp)
+"""
+
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
