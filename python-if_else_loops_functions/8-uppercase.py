84f5bcd79e14aeb8f5eaab7cd1b7c6c1c1688e1a
johnobodai <j.obodai@alustudent.com>
2022-10-21 11:24:48 +0000

    IsUpper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,13 +2,11 @@
 
 
 def uppercase(str):
-    __str = ''
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            __str = __str + chr(ord(i) - 32) 
-        else:
-            __str = __str + i
-    print("{}".format(__str), end=' ')
+            i = chr(ord(i) - 32)
+        print('{}'.format(i), end='')
+    print('')
 
 
 #uppercase('helo workld')
