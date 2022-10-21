4516739fa6f43ea248f569c2abb0fa97a2b62cce
johnobodai <j.obodai@alustudent.com>
2022-10-21 09:22:42 +0000

    To upper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    str = input("Please enter your sting to be converted")
+    str = input()
     __str = ''
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            _str = __str + chr(ord(i) - 32) 
+            __str = __str + chr(ord(i) - 32) 
         else:
             __str = __str + i
     print("{}".format(__str))
