774ffcb2328f32387c0f11a06cc122264fd68ec9
johnobodai <johnodaiobodai@gmail.com>
2022-10-21 01:05:12 +0000

    Islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-"""
-alq = input()
-alp = ord(alq)
-def islower(alp):
-    """Checks if the given argument is lowercase"""
-    if alp >= 97 and alp < 123:
-        print("{} => lower".format(alq))
-    else:
-        print("not lower")
-
-islower(alp)
-"""
 
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
