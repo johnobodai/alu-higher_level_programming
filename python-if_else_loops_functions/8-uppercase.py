28863e91e1a8a5974af27c75d15b00f91d0e3c92
johnobodai <j.obodai@alustudent.com>
2022-10-21 08:36:37 +0000

    Updated

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,1 @@
 #!/usr/bin/python3
-
-def islower(c):
-    """Checks if the given argument is lowercase"""
-    if ord('c') >= 97 and ord('c') <= 123 :
-        print("{} => lower")
-    else:
-        return False
