9ba4fddad95e95e5c070947b8f4849c660ac0313
johnobodai <j.obodai@alustudent.com>
2022-10-24 07:22:50 +0000

    Fizz Buzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def fizzbuzz():
+    for j in range(1, 101):
+        if j % 3 == 0:
+            print("Fizz", end=" ")
+        elif j % 5 == 0:
+            print("Buzz", end=" ")
+        elif j % 3 == 0 and j % 5 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print(f"{j}", end=" ")
+
+fizzbuzz()
