13c1952443ae2b35625bf03106e0e332cfc1e713
johnobodai <j.obodai@alustudent.com>
2022-10-24 10:29:28 +0000

    _Replace

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-str = "asdfhjkl"
 def remove_char_at(str, n):
-    
-    for i in range(len(str)):
+    _str = ''
+    for i in str:
         if i != n:
-            str = str.replace(str(n), '')
-        else:
-            pass
-    print(str)
+            _str = str[:n] + str[n+1:]
+    return _str
+
+
+# print(remove_char_at("qwerty", 1))
