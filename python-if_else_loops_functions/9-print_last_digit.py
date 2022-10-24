ed209d9981270eb49fa7488332975337ae77665d
johnobodai <j.obodai@alustudent.com>
2022-10-24 05:41:50 +0000

    Last digit where are you

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -6,6 +6,7 @@
 def print_last_digit(number):
     if number > 0:
         print(f"{number % 10}", end="")
+        return(number % 10)
     elif number < 0:
         print(f"{-(-number % 10)}", end="")
     else:
