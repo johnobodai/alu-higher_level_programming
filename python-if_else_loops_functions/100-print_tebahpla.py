989b531af2d0643d28dd92b182c2031d43e805d5
johnobodai <j.obodai@alustudent.com>
2022-10-24 07:58:46 +0000

    Using conditionals in print statement

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for j in range(90, 64, -1):
-    if j % 2 == 0:
-        print(f"{chr(j + 32)}", end="")
-    else:
-        print(f"{chr(j)}", end="")
+        print(f"{chr(j + 32)}" if j % 2 == 0 else f"{chr(j)}", end="")
