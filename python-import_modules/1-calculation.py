eb59153af81d6e12fead0f4b5f04a6845435cd94
johnobodai <j.obodai@alustudent.com>
2022-10-24 15:12:07 +0000

    qwerty

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,4 +4,8 @@ if __name__="__main__":
     a = 10
     b = 5
 
-add()
+print("{} + {} =".format(a, b), add(a, b))
+print("{} + {} =".format(a, b), sub(a, b))
+print("{} + {} =".format(a, b), mul(a, b))
+print("{} + {} =".format(a, b), div(a, b))
+
