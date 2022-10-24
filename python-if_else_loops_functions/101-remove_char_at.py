eef5d16c503c574684eae46b4289355ee89470e0
johnobodai <j.obodai@alustudent.com>
2022-10-24 08:50:07 +0000

    Print but n

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+str = "asdfhjkl"
+def remove_char_at(str, n):
+    
+    for i in range(len(str)):
+        if i != n:
+            str = str.replace(str(n), '')
+        else:
+            pass
+    print(str)
