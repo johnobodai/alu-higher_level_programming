dc793735d830680ffd5115cd965dc7302270de4a
johnobodai <j.obodai@alustudent.com>
2022-10-24 10:40:08 +0000

    _Replace

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -3,11 +3,11 @@
 def remove_char_at(str, n):
     _str = ''
     for i in str:
-        if i != n:
-            _str = str[:n] + str[n+1:]
-        elif i < 0:
+        if i < 0:
             _str = str
-    return _str
+        elif i != n:
+            _str = str[:n] + str[n+1:]
+           return _str
 
 
 # print(remove_char_at("qwerty", -2))
