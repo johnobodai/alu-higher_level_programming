f0d7b4bc7a7c877d9fabc62e1ec3fbb899a285bb
johnobodai <j.obodai@alustudent.com>
2022-10-24 05:56:06 +0000

    Last digit where are you

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -13,11 +13,10 @@ def print_last_digit(number):
     else:
         print(f"{number:}", end="")
         return(number)
+'''
+This function can also be used
 
-
-
-# This function can also be used
-
-# def print_last_digit(number):
-#     print(f"{abs(number) % 10}", end="")
-#     return (abs(number) % 10)
+def print_last_digit(number):
+    print(f"{abs(number) % 10}", end="")
+    return (abs(number) % 10)
+''
