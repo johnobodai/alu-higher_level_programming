f108e12d8e4d1d2e951bdc9cbbbb4b1686f2d26f
johnobodai <j.obodai@alustudent.com>
2022-10-24 08:15:32 +0000

    zYxWvUtSrQpOnMlKjIhGfEdCbA with loops in print fun

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for j in range(90, 64, -1):
-    print("{}".format(chr(j + 32) if j % 2 == 0 else "{}".format(chr(j))), end="")
+    print("{}".format(chr(j + 32) if j % 2 == 0 
+        else "{}".format(chr(j))), end="")
