13cc1a9bc1e449f11847efff402d99479e01f280
johnobodai <j.obodai@alustudent.com>
2022-10-25 10:58:32 +0000

    Updated

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("0 argument."if len(sys.argv) == 0)
+    if len(args) == 0:
+        print("Number of argument:")
+    elif len(args) > 0:
+        print("Number of arguments:")
+    for i in range(len(args)):
+        print(args(i))
+    
