d9294455c6ac6fc36f12d4410f3210ec058be7eb
johnobodai <j.obodai@alustudent.com>
2022-10-25 14:07:49 +0000

    Updated

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    if len(args) == 0:
+    import inspect, collections
+    if len(inspect.signature(collection.args)) == 0:
         print("Number of argument:")
-    elif len(args) > 0:
+    elif len(inspect.signature(collection.args)) > 0:
         print("Number of arguments:")
-    for i in range(len(args)):
+    for i in range(len(inspect.signature(collection.args))):
         print(args(i))
     
