18ab57fd01e3f166a8751bedb14ad0b7ed201c04
johnobodai <j.obodai@alustudent.com>
2022-10-26 14:30:03 +0000

    Please enter the commit message for your changes. Lines starting

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] not in j:
-        print("Unknown operator. Available operators: +, -, *, and /")
+        print("Unknown operator.Available operators: +, -, *, and /")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
--- python-import_modules/101-easy_print.py
+++ python-import_modules/101-easy_print.py
@@ -1,1 +1,3 @@
 #!/usr/bin/python3
+from sys import stderr
+stderr.write("#pythoniscool")
