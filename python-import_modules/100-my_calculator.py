19132b3ba5a50429aefc267baf0374e7b60a4e42
johnobodai <j.obodai@alustudent.com>
2022-10-26 07:29:25 +0000

    qwerty

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if sys.argv[2] == "+" or "-" or "*" or "/":
+    if sys.argv[2] != "+" or "-" or "*" or "/":
         print("Unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
     a = int(sys.argv[1])
