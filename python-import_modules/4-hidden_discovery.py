ac6a1acf40e7a239ed6fcc4460eb703861b1e78d
johnobodai <j.obodai@alustudent.com>
2022-10-26 06:14:55 +0000

    Normal hidden corrected

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,7 @@ if __name__ == "__main__":
     import hidden_4
     j = list(dir(hidden_4))
     for i in j:
-        print(f"{i}")
+        if i(1) == "_":
+            continue
+        else:
+            print(f"{i}")
