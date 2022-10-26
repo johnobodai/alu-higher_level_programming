c7c258996e44a00dae85ea89a6f88f514528f15a
johnobodai <j.obodai@alustudent.com>
2022-10-26 13:52:03 +0000

    Please enter the commit message for your changes. Lines starting

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,10 +2,11 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
+    j = {"+", "-", "*", "/"}
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if sys.argv[2] != "+" or "-" or "*" or "/":
+    if sys.argv[2] not in j:
         print("Unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
     a = int(sys.argv[1])
