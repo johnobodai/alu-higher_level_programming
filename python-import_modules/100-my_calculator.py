834219cb34527ca143aaf568e05cee9d4279aee3
johnobodai <j.obodai@alustudent.com>
2022-10-26 07:25:45 +0000

    My own calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1
+    import sys
+    if len(sys.argv) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if sys.argv[2] == "+" or "-" or "*" or "/":
+        print("Unknown operator. Available operators: +, -, *, and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    result = 0
+    print(f"<{sys.argv}> <{sys.argv[2]}> <{argv}> = <{result}>")
