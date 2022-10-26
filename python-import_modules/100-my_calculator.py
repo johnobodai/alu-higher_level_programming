666dd3623460761324d261116524c0d5bd297c8e
johnobodai <j.obodai@alustudent.com>
2022-10-26 13:24:05 +0000

    Please enter the commit message for your changes. Lines starting

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print(f"<{a}> <{sys.argv[2]}> <{b}> = <{a sys.argv[2] b>")
+    print("<{}> <{}> <{}> = <{}>".format(a, sys.argv[2], b, a sys.argv b))
