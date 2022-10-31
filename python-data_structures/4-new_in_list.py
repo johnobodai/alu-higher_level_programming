4cd6dfca5174f4e3cc936ff5c2b7cd89139c806e
johnobodai <j.obodai@alustudent.com>
2022-10-31 13:02:49 +0000

    asdkl

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,11 +3,21 @@
 
 
 def new_in_list(my_list, idx, element):
-    new_list = []
     if idx < 0 or idx > (len(new_list) - 1):
-        new_list = my_list
-        return new_list
+        return my_list
     else:
+        new_list = my_list
         new_list[idx] = element
         return new_list
 
+
+'''
+def replace_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+    else:
+        my_list[idx] = element
+        new_list = my_list
+        return new_list
+'''
