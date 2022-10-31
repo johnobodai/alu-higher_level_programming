218e167a37969085f9c43d653643c1a84bc5ecc0
johnobodai <j.obodai@alustudent.com>
2022-10-31 12:35:58 +0000

    Replace in Copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,9 +4,10 @@
 
 def new_in_list(my_list, idx, element):
     new_list = my_list
-    if idx < 0 or idx > (len(my_list) - 1):
+    if idx < 0 or idx > (len(new_list) - 1):
         return new_list
     else:
-        new_list[idx] = element
+        new_list[idx] = elementi
+        new_list = new_list
         return new_list
 
