75fe827affbe87e1e9e0a31a3ef0cc1b4376df23
johnobodai <j.obodai@alustudent.com>
2022-10-31 13:26:11 +0000

    Replace in Copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,14 +3,16 @@
 
 
 def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
     if idx < 0 or idx > (len(my_list) - 1):
-        return my_list
+        return new_list
     else:
-        new_list = my_list
         new_list[idx] = element
         return new_list
 
-
+# my_list = [1, 2, 3]
+# print(new_in_list(my_list, 1, 4))
+# print(my_list)
 '''
 def replace_in_list(my_list, idx, element):
     new_list = []
