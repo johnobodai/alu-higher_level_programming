7f3f7fc60c658766d42479d4df0c1c807fb2c47c
johnobodai <j.obodai@alustudent.com>
2022-10-31 10:51:54 +0000

    Print a list of integers

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,9 +2,9 @@
 # prints all integers of a listi
 # @i iterated all item in the givin list
 
-def pirnt_list_integers(my_list=[]):
+def print_list_integers(my_list=[]):
     for i in my_list:
         print('{:d}'.format(i))
 
 # a = [1, 2, 3, 4]
-# pirnt_list_integers(a)
+# print_list_integers(a)
