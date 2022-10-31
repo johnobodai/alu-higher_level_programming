4a0c994591a58575e196c0333adc22c263d032ac
johnobodai <j.obodai@alustudent.com>
2022-10-31 11:10:05 +0000

    Returns an element form a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -5,7 +5,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx > (len(my_list) - 1):
         return None
     else:
         return my_list[idx]
