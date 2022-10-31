c43ecb7b0284aa335d4ea6ea8674ffa615424ab0
johnobodai <j.obodai@alustudent.com>
2022-10-31 11:07:24 +0000

    Retrieves an element form a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+# retrieves an element form a list
+# returns None if negative and out of range
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
+
+
+# my_list = [1, 2, 3, 4, 5]
+# print(element_at(my_list, 3))
