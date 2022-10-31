7d043b6f3c3b25b77d5d5330d3276e625a762429
johnobodai <j.obodai@alustudent.com>
2022-10-31 12:27:07 +0000

    Replace in copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
+# Replacees an element in a list at a specific position without modifying
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list
+    if idx < 0 or idx > (len(my_list) - 1):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
+
