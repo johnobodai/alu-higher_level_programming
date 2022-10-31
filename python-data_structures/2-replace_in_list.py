db68455aa9f278ccdbd62117ad36351f6e73e4ba
johnobodai <j.obodai@alustudent.com>
2022-10-31 11:53:37 +0000

    Updated

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,7 +2,17 @@
 
 
 def replace_in_list(my_list, idx, element):
+    new_list = []
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list
     else:
         my_list[idx] = element
+        new_list = my_list
+        return new_list
+
+
+
+# a = [1, 2, 3]
+# replace_in_list(a, 1, 4)
+
+# print(replace_in_list(a, 1, 4))
