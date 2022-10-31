368fb0045d6944af283b78dfbe72b6c214d86487
johnobodai <j.obodai@alustudent.com>
2022-10-31 11:40:35 +0000

    Replace element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,9 +3,7 @@
 # returns None if negative and out of range
 
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > (len(my_list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
     else:
         return my_list[idx]
