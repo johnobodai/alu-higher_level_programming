d3147c8cf3fe2f3f4d33828886c721b81ca11d79
johnobodai <j.obodai@alustudent.com>
2022-10-31 13:03:56 +0000

    asdf

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(new_list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_list
     else:
         new_list = my_list
