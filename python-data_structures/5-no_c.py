2209bd782f4c744024f55582204913dd833d81a9
johnobodai <j.obodai@alustudent.com>
2022-10-31 13:32:46 +0000

    Pop it out

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+# Removes all cha c and C form a string
+
+def no_c(my_string):
+    for i in my_string:
+        if ord('i') = 99 or 67:
+            my_string.pop([i])
+        return my_string
