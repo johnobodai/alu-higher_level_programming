c08e71171b0a8ea34a4bbbe7f0726ec5992139d0
johnobodai <j.obodai@alustudent.com>
2022-11-01 09:04:39 +0000

    Can you C me now

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # Removes all cha c and C form a string
 
+
 def no_c(my_string):
     {ord('c'): None, ord('C'): None}
     for i in my_string:
         if ord('i') == 99 or 67:
             my_string.translate({ord('c'): None, ord('C'): None})
-    return my_string.translate({ord('c'): None, ord('C'): None}) 
+    return my_string.translate({ord('c'): None, ord('C'): None})
