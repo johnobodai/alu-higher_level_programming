3b8ba95ccb6ac09247c76595855f1327cd257709
johnobodai <j.obodai@alustudent.com>
2022-11-01 12:14:21 +0000

    sd

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,9 +6,9 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if i.index(j) != (len(i)-1):
-                print("{:d}".format(j),end=' ')
+                print("{:d}".format(j), end=' ')
             else:
-                print("{:d}".format(j),end='')
+                print("{:d}".format(j), end='')
                 print('')
 
 '''
