bdafbb1244b454b13f3f17ed49e4273bb2039b87
johnobodai <j.obodai@alustudent.com>
2022-11-01 09:00:10 +0000

    asdfg

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,8 @@
 # Removes all cha c and C form a string
 
 def no_c(my_string):
+    {ord('c'): None, ord('C'): None}
     for i in my_string:
         if ord('i') == 99 or 67:
-            del my_string[i]
+            my_string.translate({ord('c'): None, ord('C'): None})
         return my_string
