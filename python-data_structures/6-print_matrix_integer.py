4057a0572a6d38cbcd55b638b493d35f189c11b7
johnobodai <j.obodai@alustudent.com>
2022-11-01 11:45:32 +0000

    sdf

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -14,14 +14,13 @@ def print_matrix_integer(matrix=[[]]):
         print("")
 '''
 
-
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if i.index(j) != (len(i)-1):
-                print("{:d}".format(j), end=' ')
+                print("{:d}".format(j),end=' ')
             elif:
-                print("{:d}".format(j), end='')
+                print("{:d}".format(j),end='')
                 print('')
 
 '''
