0b0404ba445e27b2db3b59a3ad3a4f79195d74f9
johnobodai <j.obodai@alustudent.com>
2022-11-02 11:48:53 +0000

    Add tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -16,9 +16,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
 
+
 '''
 tuple_a = (1, 2)
 tuple_b = (1, 2)
 add_tuple(tuple_a, tuple_b)
 '''
-
