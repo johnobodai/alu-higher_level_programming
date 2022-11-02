0eb2c36771327c6fabbc3e5023799aac23af9136
johnobodai <j.obodai@alustudent.com>
2022-11-02 09:25:48 +0000

    asdf

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    maximum = my_list[0]
+    for i in my_list:
+        if i > maximum:
+            maximum = i
+    return maximum
+
+
+
+# a = [1, 90, 2, 13, 34, 5, -13, 3]
+# print(max_integer(a))
