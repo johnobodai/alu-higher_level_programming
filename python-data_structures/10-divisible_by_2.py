f5608dfb41807af66df648840c9ff87731c45dfd
johnobodai <j.obodai@alustudent.com>
2022-11-02 10:34:34 +0000

    adsf

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    result = []
+    for i in my_list:
+        if i % 2 == 0:
+            result.append(True)
+        else:
+            result.appen(False)
+    return result
