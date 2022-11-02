3e37489f0ec713ee0eb5a177eebe98025cc0c019
johnobodai <j.obodai@alustudent.com>
2022-11-02 09:38:26 +0000

    sdf

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,10 +3,10 @@
 
 
 def max_integer(my_list=[]):
-    maximum = my_list[0]
     if len(my_list) == 0:
         return None
     else:
+        maximum = my_list[0]
         for i in my_list:
             if i > maximum:
                 maximum = i
