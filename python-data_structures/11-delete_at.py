033cddaf71ccc7b07c50e1e248b98140dc1d99b5
johnobodai <j.obodai@alustudent.com>
2022-11-02 11:02:33 +0000

    sdf

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -5,5 +5,8 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list
     else:
-        my_list = [i for i in my_list if i != my_list(idx)]
-        return my_list
+        my_list = [i for i in my_list if i != my_list[idx]] 
+        return my_list#
+
+
+# print(delete_at([1, 2, 3, 4, 5], 3))
