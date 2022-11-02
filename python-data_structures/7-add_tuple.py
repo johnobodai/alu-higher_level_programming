38eb811891b7095900729f62259602c86de1dfdc
johnobodai <j.obodai@alustudent.com>
2022-11-02 08:16:54 +0000

    sdf

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,7 +5,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
     result = [x+y for x in list_a for y in list_b if list_a.index(x) == list_b.index(y)]
-    print(tuple(result))
+    return (tuple(result))
 
 '''
 tuple_a = (1, 2)
