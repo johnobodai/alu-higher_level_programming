3104e65bf1a88de0559a811309ff62153bf42568
johnobodai <j.obodai@alustudent.com>
2022-11-02 11:27:22 +0000

    Delete at

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,6 +2,7 @@
 # Deletes the item at a specific position in a list
 # @idx: the index to be deleted
 
+
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list
