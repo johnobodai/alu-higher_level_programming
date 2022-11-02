2ec08f27a5b8982a98467cb0353ab87a15063f10
johnobodai <j.obodai@alustudent.com>
2022-11-02 10:35:41 +0000

    sd

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -7,5 +7,5 @@ def divisible_by_2(my_list=[]):
         if i % 2 == 0:
             result.append(True)
         else:
-            result.appen(False)
+            result.append(False)
     return result
