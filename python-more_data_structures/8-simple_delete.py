67666cec5714c54c13cc4715f8a3623696ef6190
johnobodai <j.obodai@alustudent.com>
2022-11-03 19:04:43 +0000

    Simple delete by key

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+# Deletes a key in a dictionary
 
 
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
