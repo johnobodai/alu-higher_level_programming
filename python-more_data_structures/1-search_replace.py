8973561575580f095662c8b4b0849c50dab3d27f
johnobodai <j.obodai@alustudent.com>
2022-11-03 17:14:13 +0000

    df

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+# Replaces all occurrences of an element by another in a new list
+
+def search_replace(my_list, search, replace):
+    newlist = []
+    newlist = [replace if i == search else i for i in my_list]
+    return newlist
+
+
+'''
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+print(search_replace(my_list, 2, 89))
+'''
