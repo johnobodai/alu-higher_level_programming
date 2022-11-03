00bc42816d083754c1a38fa3af8656797a142186
johnobodai <j.obodai@alustudent.com>
2022-11-03 19:43:25 +0000

    sdf

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,13 +3,7 @@
 
 
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
-        return None
-    initial_max_key = list(a_dictionary.keys())[0]
-    initial_max_value = a_dictionary[initial_max_key]
-    for i, j in a_dictionary.items():
-        if j > initial_max_value:
-            initial_max_value = v
-            initial_max_key = i
-    return (initial_max_key)
+    if not a_dictionary:
+        return (None)
 
+    return (max(a_dictionary, key=a_dictionary.get))
