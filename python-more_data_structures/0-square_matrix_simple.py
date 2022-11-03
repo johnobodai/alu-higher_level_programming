b8dc5d05c496efd9b7f0b7f5cc4854ed133a41ca
johnobodai <j.obodai@alustudent.com>
2022-11-03 15:55:13 +0000

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+
+def square_matix_simple(matrix=[]):
+    matcpy = matrix.copy
+    matsq = []
+    for row in range(len(matrix)):
+        k = [i*i for i in matrix[row]]
+        matsq.append(k)
+    return matsq
+
+
+matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+print(square_matix_simple(matrix))
