41a076788795ea499938ca5f4e0c9c722892ca4f
johnobodai <j.obodai@alustudent.com>
2022-11-03 19:39:37 +0000

    Best Score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
+# Returns a key with the biggest integer value
+
+
+def best_score(a_dictionary):
+    if len(a_dictionary) == 0:
+        return None
+    initial_max_key = list(a_dictionary.keys())[0]
+    initial_max_value = a_dictionary[initial_max_key]
+    for i, j in a_dictionary.items():
+        if j > initial_max_value:
+            initial_max_value = v
+            initial_max_key = i
+    return (initial_max_key)
+
