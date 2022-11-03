c4a2fc8363369f6e8a0b90ca80d2d19ae3442053
johnobodai <j.obodai@alustudent.com>
2022-11-03 17:28:21 +0000

    Unique addition

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+# Adds all unique intergers in a list
+
+
+def uniq_add(my_list=[]):
+    j = list(set(my_list))
+    sum_ = 0
+    for i in j:
+        sum_ += i
+    return sum_
+   
+
+'''
+my_list = [1, 2, 3, 1, 4, 2, 5]
+print(uniq_add(my_list))
+'''
