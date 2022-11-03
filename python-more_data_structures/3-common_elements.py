1aaedd8911bd92397a851db7ebd8e8275a10b0be
johnobodai <j.obodai@alustudent.com>
2022-11-03 18:03:19 +0000

    Present in both

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
 
-def common_element(set_1, set_2):
+def common_elements(set_1, set_2):
     return set(set_1 & set_2)
+
+
+'''
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
+'''
