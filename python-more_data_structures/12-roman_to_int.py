6fecbf5ea1bf4bc65df0acc0b8e7a2fd9830eb34
johnobodai <j.obodai@alustudent.com>
2022-11-04 05:41:14 +0000

    sdf

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman_string)
+def roman_to_int(roman_string):
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         int_val = 0
         for i in range(len(s)):
