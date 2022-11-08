cdc357a5919a38f71ec579a9aa89797e8dc4ff11
johnobodai <j.obodai@alustudent.com>
2022-11-08 06:41:05 +0000

    sdf

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,11 +6,11 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-            total += 1
+            count += 1
         except IndexError:
             break
     print()      
-    return total
+    return count
 
 '''
 my_list = [1, 2, 3, 4, 5]
