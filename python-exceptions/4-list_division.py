73a8b4ec6f1548f2d7184b5063620a4fea648090
johnobodai <j.obodai@alustudent.com>
2022-11-08 08:07:57 +0000

    sdf

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,20 +3,18 @@
 
 def list_division(my_list_1, my_list_2, list_lenght):
     newlist = []
-    for i in my_list_1:
-        for j in my_list_2:
-            try:
-                if my_list_1.index(i) == my_list_2.index(j):
-                    result = i / j
-            except TypeError:
-                print("wrong type")
-                result = 0
-            except ZeroDivisionError:
-                print("division by 0")
-                result = 0
-            except IndexError:
-                print("out of range")
-                result = 0
-            finally:
-                newlist.append(result)
+    for i in range(len(list_lenght):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            newlist.append(result)
     return newlist
