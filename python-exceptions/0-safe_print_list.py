30d389ff054abb63e927f38a7571d5f9511263ba
johnobodai <j.obodai@alustudent.com>
2022-11-08 06:43:00 +0000

    Safe printing of an interger list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# Prints an interger with "{:d}.format()"
 
 
 def safe_print_list(my_list=[], x=0):
@@ -9,7 +10,7 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except IndexError:
             break
-    print()      
+    print() 
     return count
 
 '''
