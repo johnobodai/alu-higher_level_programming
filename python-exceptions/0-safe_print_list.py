7a2e991f0247af0ce87dbb9e63e6b564716b2879
johnobodai <j.obodai@alustudent.com>
2022-11-08 04:15:09 +0000

    sdf

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -9,5 +9,5 @@ def safe_print_list(my_list=[], x=0):
     except:
         pass
 
-my_list = [1, 2, 3, 4, 5]
-print(safe_print_list(my_list, 2))
+# my_list = [1, 2, 3, 4, 5]
+# print(safe_print_list(my_list, 2))
