a4b1e119dcbdd4a915f6b137182ec5ad19df2a1d
johnobodai <j.obodai@alustudent.com>
2022-11-08 06:58:05 +0000

    Safe printing of an interger value

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,18 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+# Prints an integer with "{:d:}".format()
+# Return: True is correctly printed else False
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
+
+
+'''
+value = 89
+safe_print_integer(value)
+print(safe_print_integer(value))
+'''
