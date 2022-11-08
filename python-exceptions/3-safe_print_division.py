c50c71222ec6d818f1493a13b6ea5e7e56c0eeaa
johnobodai <j.obodai@alustudent.com>
2022-11-08 07:19:56 +0000

    asdf

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@
 def safe_print_division(a, b):
     try:
         return (a / b)
-    except (ZeroDivisionError, TypeError)
+    except (ZeroDivisionError, TypeError):
         return None
     finally:
         print("Inside result: {}".format( a / b))
