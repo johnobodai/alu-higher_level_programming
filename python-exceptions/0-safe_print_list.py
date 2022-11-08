bce0b634b7f85f6aa40a2678a8b5ffc651b738bf
johnobodai <j.obodai@alustudent.com>
2022-11-08 04:14:08 +0000

    if not a_dictionary: sdf

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,14 +3,11 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        print('{}'.format(f"{i}" for i in my_list, end=''))
+        for i in range(x):
+            print("{}".format(my_list))
+
     except:
         pass
 
 my_list = [1, 2, 3, 4, 5]
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+print(safe_print_list(my_list, 2))
