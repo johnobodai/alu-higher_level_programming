8e8cc6f6141bc6b948ed32fd03cec4c4ff743ee7
johnobodai <j.obodai@alustudent.com>
2022-11-09 11:01:33 +0000

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''Define a square'''
+
+
+class Square():
+    ''' Class of square '''
+
+    def __init__(self, size):
+        ''' Initialize a new square.
+        Arguments:
+            size(int): The size of the new sqaure should be of type int '''
+        self.__size = size
