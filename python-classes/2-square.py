44849fd6d493446ab192c464d1e408f167e1def8
johnobodai <j.obodai@alustudent.com>
2022-11-09 12:23:56 +0000

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,10 +5,9 @@
 class Square():
     ''' Initialize a new square.'''
 
-    def __init__(self,size=0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an interger')
         elif size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
-
