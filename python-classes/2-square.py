2e99079cfc9e168fdd5e1591cdcb610816b2b3b5
johnobodai <j.obodai@alustudent.com>
2022-11-09 12:22:20 +0000

    Size Validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,5 +10,5 @@ class Square():
             raise TypeError('size must be an interger')
         elif size < 0:
             raise ValueError('size must be >= 0')
-        self.__size == size
+        self.__size = size
 
