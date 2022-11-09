c407ccfd88afeb3d21d10518cd969867b4085a8a
johnobodai <j.obodai@alustudent.com>
2022-11-09 13:01:18 +0000

    Acess and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
