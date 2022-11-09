7a9c4b2998c8b7ebd3b12fd213686b80662440ca
johnobodai <j.obodai@alustudent.com>
2022-11-09 10:58:37 +0000

    Size validation

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''Define a sqaure.'''
+
+
+class Square():
+    ''' Initialize a new square.'''
+
+    def __init__(self,size=0):
+        if type(size) != int:
+                raise TypeError('size must be an interger')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size == size
+
