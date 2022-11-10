bb654a704badff3855e057ff2fd862497f00125b
johnobodai <j.obodai@alustudent.com>
2022-11-10 04:34:20 +0000

    sdf

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Define a class Square."""
-
+""" Define a class Square. """
+ 
 
 class Square:
-    """Represent a square."""
+    """ Represent a square. """
 
     def __init__(self, size=0, position=(0,0)):
         self.__size = size
