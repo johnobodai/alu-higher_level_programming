07d1a8db332d5a707f5b6757c1993b245d5a7c12
johnobodai <j.obodai@alustudent.com>
2022-11-10 20:59:20 +0000

    sdf

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""Define a Rectangle class"""
+
+
+class Rectangle(self, width=0, height=0):
+    """Represent a Rectangle"""
+    def __init__():
+        self.width = width
+    
+    @property
+    def width(self):
+        self.__width = width
+
+    @property
+    def height(self):
+        self.__height = height
+
+    @setter.width
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @setter.height
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
