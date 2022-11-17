9b07a5bcd113561061825a044330ec5a6fb4c7e1
johnobodai <j.obodai@alustudent.com>
2022-11-17 10:16:44 +0000

    Same class or inherit form

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if an object is an instance of or inherited instance of a class
+    :param obj: the object to be checked
+    :type a_class: the class to be compared to
+    :returns: True if is an instace of a_class or it's inheritance
+              else False
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
