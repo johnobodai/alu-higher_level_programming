7df3032170e15a694a9a87aebf92a696bc890201
johnobodai <j.obodai@alustudent.com>
2022-11-17 11:10:56 +0000

    Only sub class of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/python3
+"""A function that that checks for inheritance"""
+
+def inherits_form(obj, a_class):
+    """Checks for inheritance
+    :param obj: the object to check
+    :type a_class: the class to check if the inherita form
+    :returns: True if the object is an insatance of a class
+              else False
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
