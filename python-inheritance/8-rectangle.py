bb5a22e1152e7c2e51e63e9e27f1ac4a01b7c595
johnobodai <j.obodai@alustudent.com>
2022-11-17 19:31:56 +0000

    Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Reprsent base geometry."""
+    
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("height", height)
+        slef.integer_validator("width", width)
+
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+        :param name: name of the parameter
+        :param value: name of parameter to validator
+        :returns TypeError if value is not an interger
+        :returns ValueError if value is <= 0>
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
