441313e53c4c124b0111483c0904618d1c998622
johnobodai <j.obodai@alustudent.com>
2022-11-18 10:58:33 +0000

    From JSON string to object

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Defines a function that returns JSON representation of an obj."""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the python representation of a JSON string.
+    :param my_obj: the string to be converted
+    :type(my_obj): string
+    :returns: python representation of a JSON string
+    """
+    return json.loads(my_str)
