3d6f5b149ac501c2d1afd8c43d2ebd0dde148006
johnobodai <j.obodai@alustudent.com>
2022-11-18 10:16:19 +0000

    write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Defines a function that writes a string to a text file."""
+
+
+def write_file(filename=", text="""):
+    """Writes a string to a text and returns num of char.
+    :param filename: the name of the file to be writen into
+    :type(filename) str:
+    :param text: the text to be writen to the file.
+    :type(text) str:
+    :returns: the number of char writeen
+    """
+    with open(filename, mode='w', encoding='utf') as f:
+        return f.write(text)
