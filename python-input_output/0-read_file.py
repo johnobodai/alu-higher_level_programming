905bbd32103e055a795268200a34a4d45f978e5a
johnobodai <j.obodai@alustudent.com>
2022-11-18 10:00:24 +0000

    0.Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+def read_file(filename=""):
+    """Print to stdout the contents of the file."""
+    with open(filename, mode 'r', encoding="utf-8") as openfile:
+        print(openfile.read(), end='')
