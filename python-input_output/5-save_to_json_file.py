657f4d6ac72bf8ea642b85f256dd98e05fd3e3d7
johnobodai <j.obodai@alustudent.com>
2022-11-18 12:14:58 +0000

    Save Objext to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Defines a functio that writes an Obj to a text file with JSON"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes as Odj to a text file using JSON repr"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
