3f4f5c73a8b48060a0976b712e7cbb063a66b0bd
johnobodai <j.obodai@alustudent.com>
2022-11-22 12:05:58 +0000

    What's my status?#1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -3,11 +3,11 @@
 - https://alu-intranet.hbtn.io/status
 """
 
-import urllib.request
+import urllib.requests
 
 if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
-    with urllin.request.urlopen(url) as res:
+    with urllin.requests.urlopen(url) as res:
         body = res.read()
         print("Body response:")
         print("\t- type: type: {}".format(type(display)))
