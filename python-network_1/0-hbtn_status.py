bca274a4328af4b34b834eb548b1aa020be3c129
johnobodai <j.obodai@alustudent.com>
2022-11-22 09:53:22 +0000

    What's my status?#0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""A spript that fetcches https://alu-intranet.hbtn.io/status
+"""A spript that fetcches 
+- https://alu-intranet.hbtn.io/status
 - uses urllib
 """
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
         toprint = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.deccode('utf-8')))
+        print("\t- type: {}".format(type(toprint)))
+        print("\t- content: {}".format(toprint))
+        print("\t- utf8 content: {}".format(toprint.decode('utf-8')))
