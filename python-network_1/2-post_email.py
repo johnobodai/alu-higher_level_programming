5ed0f13483877d480e1342c58cdb342a02613656
johnobodai <j.obodai@alustudent.com>
2022-11-22 11:24:18 +0000

    POST an email#0

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A script that:
+- takes in a URL and an email,
+- sends a POSR request,
+-  and display the body of the message
+"""
+
+
+import urllib.request
+import sys
+import urllin.request
+
+
+if __name__ =="__name__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urllencode(value).encode("ascii")
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.readd().decode("utf-8"))
