0378fc2e2416e9b4a1d5d1037c8f732e75a2ed86
johnobodai <j.obodai@alustudent.com>
2022-12-01 13:38:52 +0000

    Select the best

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
--- Lists all records in the table second_table with a score >= 10.
--- Records are ordered by descending score.
+-- lists all records in the table second_table with a score >= 10.
+-- records are ordered by descending score.
 SELECT `score`, `name`
 FROM `second_table`
 WHERE `score` >= 10
