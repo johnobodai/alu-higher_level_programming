fd88b9fbc7211e8d2a51e2eeb20ff54436062414
johnobodai <j.obodai@alustudent.com>
2022-12-01 14:02:16 +0000

    Temperature #0

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
--- Displays the average temperature (in Fahrenheit)
+-- displays the average temperature (in Fahrenheit)
 -- by city ordered by descending temperature.
 SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
