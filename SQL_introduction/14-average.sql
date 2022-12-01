725c44d50b663f99fd0b9c62c3b17d5cd270b510
johnobodai <j.obodai@alustudent.com>
2022-12-01 13:57:41 +0000

    Average

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
--- computes the average score of all records in the table second_table.
+-- computes the average score of all records in the table second_table
 SELECT AVG(`score`) AS `average`
 FROM `second_table`;
