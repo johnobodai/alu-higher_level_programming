383d268ced857567286a63e1f89d97d6f12f876c
johnobodai <j.obodai@alustudent.com>
2022-12-01 13:37:47 +0000

    List by best

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
--- Lists all records of the table second_table.
--- Records are ordered by descending score.
+-- lists all records of the table second_table.
+-- records are ordered by descending score.
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
