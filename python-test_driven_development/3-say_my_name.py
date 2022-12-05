058f3e38e2afa1de406e5cf9f67b5dfc96409187
johnobodai <j.obodai@alustudent.com>
2022-12-05 11:18:52 +0000

    Say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Defines name printing function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Print the inputed name
+    
+    :param: first_name 
+    :type first_name: string
+    :raise: TypeError if first_name not string
+    :param: last_name
+    :type last_name: string
+    :raises: TypeError if last_name not string
+    :returns: none
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
