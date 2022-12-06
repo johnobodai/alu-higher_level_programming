b3e0cef4504cdc675f24fc15b3e2cc7c64a6fc0d
johnobodai <j.obodai@alustudent.com>
2022-12-06 08:38:26 +0000

    dsf

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest classes:
+    TestBase_instantiation
+    TestBase_to_json_string
+    TestBase_save_to_file
+    TestBase_from_json_string
+    TestBase_create
+    TestBase_load_from_file
+    TestBase_save_to_file_csv
+    TestBase_load_from_file_csv
+"""
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
