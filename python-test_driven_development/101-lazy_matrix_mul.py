d87d12754b0463f5cbccd040b13721d01a4d5505
johnobodai <j.obodai@alustudent.com>
2022-12-07 03:52:58 +0000

    Lazy multiplication

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Defines a matrix multiplication function using Numpy"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Matrix multiplication using Numpy
+
+    :param m_a: The first matrix
+    :type m_a: list of list of ints/floats
+    :param m_b: The second matrix
+    :type m_b: list of list of int/floats
+    :returns: The results of the matrix multiplication
+    :rtype: list of list of int/floats
+    """
+    
+    return(numpy.matmul(m_a, m_b))
