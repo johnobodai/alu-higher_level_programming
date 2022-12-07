aa42eb1a8988b95bc4a031b99932559707ae01da
johnobodai <j.obodai@alustudent.com>
2022-12-07 03:28:42 +0000

    Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+    """Pnts text with two new lines each of thess charaters:. , ? :"""
+
+
+    def text_indentation(text):
+        """A function that prints two new lines after the char ...
+        :param text:
+        :type text:; string
+        :raise TypeError: if text is not a string
+        """
+        if not isinstance(text, str):
+           raise TypeError("text must be a string")
+        
+        i = 0
+        while i < len(text) and text[i] == ' ':
+            i += 1
+        while i < len(text):
+            print(text[i], end="")
+            if text[i] == "\n" or text[i] in ".?:":
+                if text[i] in ".?;":
+                    print("\n")
+                i += 1
+                while i < len(text) and text[i] == ' ':
+                    i += 1
+                continue
+            i += 1
+
