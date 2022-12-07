96bf748c03a8030cc7750db61c4efd6c3dabd998
johnobodai <j.obodai@alustudent.com>
2022-12-07 04:32:08 +0000

    Divide a matrix py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,19 +3,19 @@
 
 
 def matrix_divided(matrix, div):
-    """Divide all elements of a matrix.
+    """Divides all element of a matrix
 
-    Args:
-        matrix (list): A list of lists of ints or floats.
-        div (int/float): The divisor.
-    Raises:
-        TypeError: If the matrix contains non-numbers.
-        TypeError: If the matrix contains rows of different sizes.
-        TypeError: If div is not an int or float.
-        ZeroDivisionError: If div is 0.
-    Returns:
-        A new matrix representing the result of the division.
+    :param matrix: The matrix(list of list) whose element are to be divided by div
+    :type matrix: list
+    :param div: The value that divides each element in the matrix
+    :type div: int
+    :returns: A new matrix (list of list), derived form the division
+    :rtype: list(list of lists)
+    :raise: TypeError if div is not an int
+    :raise: TypeError if matrix is not a list of lists
+    :raise: TypeError if each row of the matrix is not of the same size
     """
+
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
             not all((isinstance(ele, int) or isinstance(ele, float))
