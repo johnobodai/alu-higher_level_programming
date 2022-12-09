5aa525285ad4f48c2529290ae40444bf5986df8e
johnobodai <j.obodai@alustudent.com>
2022-12-09 05:19:33 +0000

    Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,7 +11,7 @@ class Base:
         :param id: The idintity of the new Base
         :type id: int
         """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
