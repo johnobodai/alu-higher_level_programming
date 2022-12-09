880ab866c751350af60f2027e4df66377340f65e
johnobodai <j.obodai@alustudent.com>
2022-12-09 06:27:48 +0000

    First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""This module is the of the Rectangle"""
+import base.py
+
+class Rectangle(base.Base()):
+    """Represent a rectangle."""
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialise a new Rectangle
+        :param x: The x coordinate of the new Rectangle
+        :type x: int
+        :param y: The y coordinate of the new Rectangle
+        :type y: int
+        :param id: The identity of the new Rectangle
+        :type id: int
+        """
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+        @property
+        def width(self):
+            """Set the widthe of the Rectangle."""
+            return self.__width
+
+        @property
+        def height(self):
+            return self.__height
+
+        @property
+        def x(self):
+            return self.__x
+        @property
+        def y(self):
+            return self.__y
+
+        @width.setter
+        def width(self, value):
+            self.__width = value
+        
+        @height.setter
+        def height(self, value):
+            self.__height = value
+
+        @x.setter
+        def x(self, value):
+            self.__x = value
+
+        @y.setter
+        def x(self, value):
+            self.__y = value
+
