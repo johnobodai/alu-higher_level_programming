25b18d6c72df79ea9839a04808ad33e6916e697f
johnobodai <j.obodai@alustudent.com>
2022-12-09 05:18:06 +0000

    Base clas

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This modules defines a Base class function"""
+
+
+class Base:
+    """This class is the base class of these module"""
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Initialize a new Base
+        :param id: The idintity of the new Base
+        :type id: int
+        """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
