9ad81eb7f7093e079cf4adb5272389f32644669f
johnobodai <j.obodai@alustudent.com>
2022-12-12 12:48:54 +0000

    se

--- python-almost_a_circle/tests/base.py
+++ python-almost_a_circle/tests/base.py
@@ -7,7 +7,20 @@ import turtle
 
 class Base:
     """Represent the base model for all the classes in this porject
+    
     :attr __nb_objects: The number of instantiated Bases.
+    :type __nb_objects: int
     """
 
     __nb_objects = 0
+    def __init__(self, id=None):
+        """Initialise a new Base.
+
+        :param id: The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else Base.__nb_objects += 1
+        self.id = Base.__nb_objects
+
+    @
