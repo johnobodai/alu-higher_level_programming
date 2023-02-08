1a0a2c9af2174c8392fda1d94809c34547ff2181
johnobodai <j.obodai@alustudent.com>
2023-02-08 04:55:41 +0000

    whatspace removed

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-/* a script that prints My number: <first argument converted in integer> 
+/* a script that prints My number: <first argument converted in integer>
  * if the first argument can be converted to an integer:
  */
 
