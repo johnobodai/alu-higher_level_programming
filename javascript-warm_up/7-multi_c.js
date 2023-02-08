5846153944c3c247e32d5093d0ea523fbadbadc8
johnobodai <j.obodai@alustudent.com>
2023-02-08 08:08:06 +0000

    indentation correction

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < x; i++){
-      console.log('C is fun');
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
     }
 }
