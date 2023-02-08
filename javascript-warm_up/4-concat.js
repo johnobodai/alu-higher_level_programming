cd35cb6c98692c4d0fd925d4a1725c9a068efbdd
johnobodai <j.obodai@alustudent.com>
2023-02-08 03:52:51 +0000

    concatination

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 
-console.log(process.argv.length === 4 ? process.argv[3] 'is' process.argv[4] : process.argv.length === 3 ? process.argv[3] 'is undefined' : 'undefined is undefined' )
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
