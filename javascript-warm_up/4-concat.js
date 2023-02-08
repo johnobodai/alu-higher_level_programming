da52b124092a319214d05c43c1e879c2e07f617d
johnobodai <j.obodai@alustudent.com>
2023-02-08 04:18:48 +0000

    commented

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
+/*a script that prints two arguments passed to it, in the following format: “ is ”
+    You must use console.log(...) to print all output
+    You are not allowed to use var
+
+*/
 
 console.log(`${process.argv[2]} is ${process.argv[3]}`);
