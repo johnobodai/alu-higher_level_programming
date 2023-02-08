00412022d1d4b0a387c002300044d5b076dd914e
johnobodai <j.obodai@alustudent.com>
2023-02-08 07:41:29 +0000

    loop to languages

---
+++ javascript-warm_up/6-milti_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* this code loops through the dictionary or objects 
+ * and logs then on the console
+ */
+
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
+}
