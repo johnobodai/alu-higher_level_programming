60c7660ca56beccb5a77591637642a7d9a865660
johnobodai <j.obodai@alustudent.com>
2023-02-09 14:27:07 +0000

    indentation applied

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,11 +4,11 @@ const integerNum = Math.floor(Number(process.argv[2]));
 if (isNaN(integerNum)) {
   console.log('Missing size');
 } else {
-    for (let r = 0; r < integerNum; r++) {
-      let row = '';
-        for (let c = 0; c < integerNum; c++) {
-          row += 'X';
-        }
-        console.log(row);
+  for (let r = 0; r < integerNum; r++) {
+    let row = '';
+    for (let c = 0; c < integerNum; c++) {
+      row += 'X';
     }
+        console.log(row);
+  }
 }
