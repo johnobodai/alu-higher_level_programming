713f34799447abaf129014db90bb19c690ba4af6
johnobodai <j.obodai@alustudent.com>
2023-02-10 21:22:53 +0000

    scrap

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
+// A script that gets the contents of a webpage and stores it in a file
 
-const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
