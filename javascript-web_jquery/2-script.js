98c563b5618b9ea2549bfe140991ec2b6f12bc4d
johnobodai <j.obodai@alustudent.com>
2023-02-10 21:41:06 +0000

    jq

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -1,6 +1,3 @@
-// JavaScript script that updates the text color of the <header> element to red (#FF0000) 
-// when the user clicks on the tag DIV#red_header
-
 $('DIV#red_header').click(function () {
-    $('header').css({ color: '#FF0000' });
+  $('HEADER').css('color', '#FF0000');
 });
